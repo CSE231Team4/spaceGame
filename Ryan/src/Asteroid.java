2b5c51d4040762cef262d236512fae32c97b67fd
rmkomeshak <rmkomeshak@oakland.edu>
2016-10-16 21:51:01 -0400

    Added powerup (currently doesn't do anything)
    Also reformatted the resource folder so it has Fonts and Images so the URLs changed

--- Ryan/src/Asteroid.java
+++ Ryan/src/Asteroid.java
@@ -8,12 +8,17 @@ import com.sun.javafx.sg.prism.NGNode;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 
 public class Asteroid extends Node implements Obstacle {
     private int width; //width of the image
     private int height; //height of the image
-    private Image asteroid_image = new Image("file:resource/asteroid.png", true); //sets the path of the asteroid image
-    private ImageView astImage = new ImageView(asteroid_image); //sets up the imageviewer for the asteroid image
+    private int lane;
+    private int guessArt;
+    private Image asteroid_image1 = new Image("file:resource/Images/asteroid.png", true);
+    private Image asteroid_image2 = new Image("file:resource/Images/asteroid2.png", true); //sets the path of the asteroid image
+    private ImageView astImage = new ImageView(asteroid_image1); //sets up the imageviewer for the asteroid image
+    private ImageView ast2Image = new ImageView(asteroid_image2);
     
     final int EDGE_OBSTACLE_GAP = 15; //the gap on the edge is 5px larger than the ship gap because it takes 5px extra on one side
     final int MID_OBSTACLE_GAP = EDGE_OBSTACLE_GAP + 5; //the gaps beween lanes in the middle are 5px bigger because it takes 5 off both sides
@@ -24,19 +29,29 @@ public class Asteroid extends Node implements Obstacle {
         width = 95;
         height = 95;
         //default width and height are 95 because that's what we're using, but this is subject to change
+        lane = 0;
     }
     
     public Asteroid(int w, int h){
         super();
         width = w;
         height = h;
+        lane = 0;
     }
     
     public ImageView initAsteroidGraphics(){
+        int guessAst = (int) (Math.random() * 100);
+        if (guessAst % 2 == 1) {
         astImage.setFitWidth(width);
         astImage.setFitHeight(height);
         //sets the width and height of the image
         return astImage;
+        }
+        else {
+            ast2Image.setFitWidth(width);
+            ast2Image.setFitHeight(height);
+            return ast2Image;
+        }
     }
     
     public int getWidth(){
@@ -54,6 +69,10 @@ public class Asteroid extends Node implements Obstacle {
     public int getMidGap(){
         return MID_OBSTACLE_GAP;
     }
+    
+    public int getLane(){
+        return lane;
+    }
         
     public void setWidth(int w){
         width = w;
@@ -61,6 +80,10 @@ public class Asteroid extends Node implements Obstacle {
     
     public void setHeight(int h){
         height = h;
+    }
+    
+    public void setLane(int l){
+        lane = l;
     }
     
     @Override
--- Ryan/src/SpaceX33.java
+++ Ryan/src/SpaceX33.java
@@ -1,5 +1,8 @@
 package spacex33;
 
+import java.awt.Button;
+import java.awt.Label;
+import java.io.InputStream;
 import javafx.animation.AnimationTimer;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
@@ -14,28 +17,68 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javafx.animation.TranslateTransition;
+import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 
 public class SpaceX33 extends Application {   
+    private Scene gameScene;
     private AnimationTimer timer; 
     private Pane root; //declare the root pane
     private List<Node> asteroids = new ArrayList<>(); //array list of asteroids
+    private List<Node> powerups = new ArrayList<>(); //array list of powerups
+    private int ship_lane;
+    private double rand_hold = 0;
     private Node shipDisplay = new Ship(); //create the display node for the ship
-    private ImageView astImg = new ImageView(); //create the imageviewer for the ship
-    private Ship shipObj = new Ship();
+    private Node heartDisplay1 = new Heart();
+    private Node heartDisplay2 = new Heart();
+    private Node heartDisplay3 = new Heart();
+    private ImageView astImg = new ImageView(); //create the imageviewer for the asteroids
+    private ImageView slowImg = new ImageView();
+    private final Ship shipObj = new Ship();
+    private final SlowTime slowObj = new SlowTime();
+    
+    private final Heart heartObj1 = new Heart();
+    private final Heart heartObj2 = new Heart();
+    private final Heart heartObj3 = new Heart();
+    
+    private boolean enableShip = true;
     
     final int NUM_LANES = 6; //number of lanes for obstacles to spawn
-    
+    final int WINDOW_WIDTH = 700;
+    final int WINDOW_HEIGHT = 900;
+    private int speed = 5; //speed of asteroids descending toward ship
+    private int spawnCount = 0; //counts amount of asteroids spawned in
+    private double speedAdd = 0; //used in algorithm to calculate difficulty. speedAdd = 1/10 of spawnCount
+    private double spawnRate = 0.01; //spawn in speed of asteroids
+    private int current_lane;
+    private int iframes = 0;
+    private long istart = 0;
+    private int shipFlash = 0;
+    private long SHIP_FLASH_START = 0;
+    private Node toRemAst = new Asteroid();
+    private Node toRemSlow = new SlowTime();
+
     private Parent createContent(){
         root = new Pane(); //initialize the pane
-        root.setPrefSize(700, 900); //set the size of the pane
+        root.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT); //set the size of the pane
         root.setId("pane"); //set the ID of the pane for the CSS file
-        
-        shipDisplay = initShip(); //calls the initShip method to load in the graphics for the ship
+        shipDisplay = shipObj.initShipGraphics(); //calls the initShip method to load in the graphics for the ship
+        heartDisplay1 = heartObj1.initHeartGraphics();
+        heartDisplay2 = heartObj2.initHeartGraphics();
+        heartDisplay2.setTranslateX(80);
+        heartDisplay3 = heartObj3.initHeartGraphics();
+        heartDisplay3.setTranslateX(155);
         
         root.getChildren().add(shipDisplay); //adds the shipDisplay to the root so it can be displayed
+        root.getChildren().add(heartDisplay1);
+        root.getChildren().add(heartDisplay2);
+        root.getChildren().add(heartDisplay3);
+        
         
         timer = new AnimationTimer() {
             @Override
@@ -48,20 +91,40 @@ public class SpaceX33 extends Application {
         return root;  
     }
     
-    private ImageView initShip(){
-        return shipObj.initShipGraphics(); //gets the graphic loaded in the Ship class
-    }
-    
-    private Node spawnObstacle() {
+    private Node spawnAsteroid() {
         Asteroid ast = new Asteroid();
+        
         astImg = ast.initAsteroidGraphics(); //gets the graphic loaded in the Asteroid class
+        spawnCount++;
         
         int lane = getLane(); //gets a lane number between 0-5
+        
+        current_lane = lane;
         int obs_location = ast.getEdgeGap() + lane * (ast.getWidth() + ast.getMidGap()); //formula for calculating the X coordinate of the obstacle
         astImg.setTranslateX(obs_location); //sets the x coordinate of the obstacle to obs_location
-
         root.getChildren().add(astImg); //adds the graphic for the asteroid to the root
+        refreshHUD();
+        
+
         return astImg;
+    }
+    
+    private Node spawnPowerup(){
+        SlowTime st = new SlowTime();
+        slowImg = st.initSlowGraphics();
+        int plane = getLane();
+        int p_location = st.getEdgeGap() + plane * (st.getWidth() + st.getMidGap());
+        slowImg.setTranslateX(p_location);
+        root.getChildren().add(slowImg);
+        refreshHUD();
+        
+        return slowImg;
+    }
+    
+    private void refreshHUD(){
+        heartDisplay1.toFront();
+        heartDisplay2.toFront();
+        heartDisplay3.toFront();
     }
     
     private int getLane(){
@@ -70,77 +133,144 @@ public class SpaceX33 extends Application {
     
     private void onUpdate() {
         for (Node asteroid : asteroids){
-            asteroid.setTranslateY(asteroid.getTranslateY() + 12); //translates the asteroid down 12 pixels every update
-            if(asteroid.getTranslateY() > 850){
-                asteroid = null;
-                asteroids.remove(asteroid);
-                //this was my attempt at saving memory... doesn't work too well but makes it slightly more efficient
+            asteroid.setTranslateY(asteroid.getTranslateY() + speed); //translates the asteroid down 12 pixels every update
+            //asteroid.setRotate(asteroid.getRotate() + speed);
+            if(asteroid.getTranslateY() > 900){
+                toRemAst = asteroid;
+                root.getChildren().remove(asteroid);
             }
         }
-
-        if (Math.random() < 0.03) {
-            asteroids.add(spawnObstacle()); //randomly spawns an obstacle, and adds the graphic for the asteroid to the asteroid list
+        asteroids.remove(toRemAst);
+        
+        for (Node powerup : powerups){
+            powerup.setTranslateY(powerup.getTranslateY() + speed); //translates the asteroid down 12 pixels every update
+            //asteroid.setRotate(asteroid.getRotate() + speed);
+            if(powerup.getTranslateY() > 900){
+                toRemSlow = powerup;
+                root.getChildren().remove(powerup);
+            }
         }
+        powerups.remove(toRemSlow);
+        
+        if (spawnRate < 2.0) {//sets limit on spawning
+            if (spawnCount == 25) { //initial condition to add to spawnRate
+                spawnRate = spawnRate + 0.005; //add to spawnRate if conditions are met
+                spawnCount = 0; // reset spawn counter
+                speedAdd = speedAdd + 0.2; // add 1 to speed add for every 10 spawn count
+                if (speedAdd == 1) { //condition to add to speed of obsticles
+                    speed = speed + 5; //add 5 to speed of falling obsticles
+                    speedAdd = 0; //reset speed add counter
+                }
+            }   
+        }
+        
+        rand_hold = Math.random();
+        
+        if (rand_hold < 0.03 && rand_hold > 0.002) {
+            asteroids.add(spawnAsteroid()); //randomly spawns an obstacle, and adds the graphic for the asteroid to the asteroid list
+        }
+        if(rand_hold < 0.002)
+            powerups.add(spawnPowerup());
 
         checkState();
     }
     
-    private void checkState() {
+    private void checkState(){
         for (Node asteroid : asteroids) {
-            if (asteroid.getBoundsInParent().intersects(shipDisplay.getBoundsInParent())) { //checks for an intersection between the asteroid and the ship
-                timer.stop(); //stops the timer so asteroids no longer spawn
-                shipDisplay.setVisible(false); //sets the ship to be invisible (most likely will be changed, but this is the current solution)
+            if (System.currentTimeMillis() - istart > iframes) {
+                shipObj.checkShipState();
+                iframes = 0;
+                if (isCollision(asteroid)) { //checks for an intersection between the asteroid and the ship 
+                    iframes = 2000;
+                    istart = System.currentTimeMillis();
+                    //asteroids.remove(asteroid);
+                    root.getChildren().remove(asteroid);
+                    
+                    shipObj.setHealth(shipObj.getHealth()-1);
+                    shipObj.setShipState();
                 
-                String lose = "YOU LOSE";
-                HBox hBox = new HBox();
-                hBox.setTranslateX(250); //sets x coordinate of the HBox
-                hBox.setTranslateY(420); //sets y coordinate of the HBox
-                //I couldn't figure out how to center the text and just tried to eyeball it, if anyone has any fixes
-                root.getChildren().add(hBox); //adds the hBox object to the root
+                    switch(shipObj.getHealth()){
+                        case 0: heartDisplay1.setVisible(false);
+                                break;
+                        case 1: heartDisplay2.setVisible(false);
+                                break;
+                        case 2: heartDisplay3.setVisible(false);
+                                break;
+                        default:
+                    }
                 
-                for (int i = 0; i < lose.toCharArray().length; i++) {
-                    char letter = lose.charAt(i);
-
-                    Text text = new Text(String.valueOf(letter));
-                    text.setFont(Font.font(50));
-                    text.setOpacity(0);
-                    text.setFill(Color.WHITE);
-
-                    hBox.getChildren().add(text);
-
-                    FadeTransition ft = new FadeTransition(Duration.seconds(0.66), text);
-                    ft.setToValue(1);
-                    ft.setDelay(Duration.seconds(i * 0.15));
-                    ft.play();
+                    if(shipObj.getHealth() <= 0){
+                        enableShip = false;
+                        timer.stop(); //stops the timer so asteroids no longer spawn
+                
+                        String lose = "YOU LOSE";
+                        HBox hBox = new HBox();
+                        hBox.setPrefSize(700, 900);
+                        hBox.setAlignment(Pos.CENTER);
+                        //hBox.setTranslateX(180); //sets x coordinate of the HBox
+                        //hBox.setTranslateY(420); //sets y coordinate of the HBox
+                        //I couldn't figure out how to center the text and just tried to eyeball it, if anyone has any fixes
+                        root.getChildren().add(hBox); //adds the hBox object to the root
+                
+                        for (int i = 0; i < lose.toCharArray().length; i++) {
+                            char letter = lose.charAt(i);
+                            Text text = new Text(String.valueOf(letter));
+                            //text.setFont(Font.font(50));
+                            text.setFont(Font.loadFont("file:resource/Fonts/PressStart2P.ttf", 40));
+                            
+                            text.setOpacity(0);
+                            text.setFill(Color.WHITE);
+                            hBox.getChildren().add(text);
+                            FadeTransition ft = new FadeTransition(Duration.seconds(0.66), text);
+                            ft.setToValue(1);
+                            ft.setDelay(Duration.seconds(i * 0.15));
+                            ft.play();
+                        }
+                    }
+                    //the above section is used for printing the "YOU LOSE" font
+                    return;
                 }
-                //the above section is used for printing the "YOU LOSE" font
-                return;
+            }
+            else {
+                iframes--;
             }
         }
+        
+        for(Node powerup : powerups){
+            if(isCollision(powerup)){
+                root.getChildren().remove(powerup);
+            }
+        }
+}
+    
+    private boolean isCollision(Node hb){
+        return hb.getBoundsInParent().intersects(shipDisplay.getBoundsInParent()); //returns whether or not the bounds intersect
     }
     
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(createContent());
-        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm()); //sets the stylesheet of the scene (used for the background image)
+        gameScene = new Scene(createContent());
+        gameScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm()); //sets the stylesheet of the scene (used for the background image)
+       
+        stage.setScene(gameScene); 
+        stage.setResizable(false); //makes it not possible to resize the window since this breaks the game
         
-        stage.setScene(scene); 
-        //stage.setResizable(false); //makes it not possible to resize the window since this breaks the game
-        //BUG: with this enabled, the asteroids look like the overlap the edge of the window, check it yourself
-        
-        TranslateTransition tt = new TranslateTransition(Duration.millis(100), shipDisplay); //sets up the ship to slide over the course of 100ms (0.1s). Change the Duration.millis to make the animation slower or faster
+        TranslateTransition shipSlide = new TranslateTransition(Duration.millis(100), shipDisplay); //sets up the ship to slide over the course of 100ms (0.1s). Change the Duration.millis to make the animation slower or faster
+        ship_lane = 0;
         stage.getScene().setOnKeyPressed(event -> {
             switch (event.getCode()) {
                 case A:
-                    if(shipDisplay.getTranslateX() != shipObj.getGap()){ //checks if the ship is not at the edge (10px away)
-                        tt.setByX(-(shipObj.getWidth() + shipObj.getGap())); //formula to calculate position the ship should slide to (negative becuse moving to the left)
-                        tt.play(); //plays animation
+                    if(shipDisplay.getTranslateX() != shipObj.getGap() && enableShip){ //checks if the ship is not at the edge (10px away)
+                        shipSlide.setByX(-(shipObj.getWidth() + shipObj.getGap())); //formula to calculate position the ship should slide to (negative becuse moving to the left)
+                        shipSlide.play(); //plays animation
+                        ship_lane--;
                     }
                     break;
                 case D:
-                    if(shipDisplay.getTranslateX() != (700-shipObj.getGap()-shipObj.getWidth())){ //700 (total window size) - one gap at the furthermost edge - size of one ship
-                        tt.setByX(shipObj.getWidth() + shipObj.getGap()); //same formula as previously but positive
-                        tt.play(); //plays animation
+                    if(shipDisplay.getTranslateX() != (WINDOW_WIDTH-shipObj.getGap()-shipObj.getWidth()) && enableShip){ //700 (total window size) - one gap at the furthermost edge - size of one ship
+                        shipSlide.setByX(shipObj.getWidth() + shipObj.getGap()); //same formula as previously but positive
+                        shipSlide.play(); //plays animation
+                        ship_lane++;
                     }
                     break;
                 default:
